c55f964cb2c5c2ff6fe39ef801639dad595a9856
Inigo Palileo <inigo.palileo@gmail.com>
2012-10-04 22:12:21 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giftly::Application.config.secret_token = '56562cd61b5e406e59f4c611bc1d5b4a32d47d80213b3ae17e564b04af2e09f08941f00a1ddfd00034a31e6c329b66d89787c372d08acf8ff5d840ea022e28cf'
